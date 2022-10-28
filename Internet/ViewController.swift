2ab08f3395b8975751e82ef5f449012d47f0a1bc
KevinHdz <kevinhdcz@yahoo.com.mx>
2022-10-28 17:30:24 -0500

    Initial Commit

---
+++ Internet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Internet
+//
+//  Created by Kevin Hernández on 28/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
